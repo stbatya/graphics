2f2b90c79fb86eedca9ae744da4010a566690803
k <punk.stum@gmail.com>
2021-09-09 14:46:07 +0300

    changed query from orm to execute-style

--- flaskr/main.py
+++ flaskr/main.py
@@ -37,7 +37,7 @@ def pyt():
     #picture['scat'] = graph_scat(data_2,'bmi','charges','smoker')
     #query table
     s = db.session.execute(select(Insurance))
-    column_lst = [i['name'] for i in s.column_descriptions]
+    column_lst = [i for i in s.keys()]
     query_dict = {name:[j[name] for j in s.mappings()] for name in column_lst}
     data_2 = pd.DataFrame.from_dict(query_dict)
     #data_2 = pd.DataFrame.from_records(s.all())
